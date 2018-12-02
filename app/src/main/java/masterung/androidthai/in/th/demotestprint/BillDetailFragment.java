ab76c862fe81add6e076a10f7830e4e882c69de4
masterUNG <phrombutr@gmail.com>
2018-12-02 16:02:09 +0700

    RecyclerView Detail Success

--- app/src/main/java/masterung/androidthai/in/th/demotestprint/BillDetailFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/demotestprint/BillDetailFragment.java
@@ -4,10 +4,17 @@ package masterung.androidthai.in.th.demotestprint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 /**
@@ -48,13 +55,38 @@ public class BillDetailFragment extends Fragment {
 
     private void createDetail() {
 
+        RecyclerView recyclerView = getView().findViewById(R.id.recyclerViewBillDetail);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        ArrayList<String> nameStringArrayList = new ArrayList<>();
+        ArrayList<String> detailStringArrayList = new ArrayList<>();
+        ArrayList<String> amountStringArrayList = new ArrayList<>();
+        ArrayList<String> billStringArrayList = new ArrayList<>();
+        ArrayList<String> priceStringArrayList = new ArrayList<>();
+
 
         try {
 
             GetDetailBillWhereID getDetailBillWhereID = new GetDetailBillWhereID(getActivity());
-            getDetailBillWhereID.execute(idBillString , myConstant.getUrlBillDetailWhereOID());
+            getDetailBillWhereID.execute(idBillString, myConstant.getUrlBillDetailWhereOID());
             String jsonString = getDetailBillWhereID.get();
             Log.d(tag, jsonString);
+
+            JSONArray jsonArray = new JSONArray(jsonString);
+            for (int i = 0; i < jsonArray.length(); i += 1) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                nameStringArrayList.add(jsonObject.getString("name"));
+                detailStringArrayList.add(jsonObject.getString("des"));
+                amountStringArrayList.add(jsonObject.getString("num"));
+                billStringArrayList.add(jsonObject.getString("setpr"));
+                priceStringArrayList.add(jsonObject.getString("price"));
+            }
+
+            BillDetailAdapter billDetailAdapter = new BillDetailAdapter(getActivity(), nameStringArrayList,
+                    detailStringArrayList, amountStringArrayList, billStringArrayList, priceStringArrayList);
+            recyclerView.setAdapter(billDetailAdapter);
+
 
         } catch (Exception e) {
             Log.d(tag, "e at createDetail ==> " + e.toString());
